cfd33aba27482aea11870e0a350c7a9fc1d93dc3
dusklight00 <r.rahul.developer@gmail.com>
2024-02-09 18:24:40 +0530

    custom connectivity issue resolved #4

--- frontend-v2/src/App.jsx
+++ frontend-v2/src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <>hello</>;
+  return <div>Hello World</div>;
 }
 
 export default App;
--- frontend-v2/src/main.jsx
+++ frontend-v2/src/main.jsx
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Canvas from "./components/ReactFlow/Canvas";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <Canvas />
+  </StrictMode>
 );
