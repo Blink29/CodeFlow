962c25c88cb6bda1e0bfaa32a9d539bbbfa49607
dusklight00 <r.rahul.developer@gmail.com>
2024-02-14 08:02:17 +0530

    react flow graph integration complete with backend output

--- frontend-v2/src/components/ReactFlow/initial-elements.jsx
+++ frontend-v2/src/components/ReactFlow/initial-elements.jsx
@@ -84,32 +84,37 @@ export const nodes = [
 ];
 
 export const edges = [
-  { id: "e1-2", source: "1", target: "2", label: "this is an edge label" },
-  { id: "e1-3", source: "1", target: "3", animated: true },
-  {
-    id: "e4-5",
-    source: "4",
-    target: "5",
-    type: "smoothstep",
-    sourceHandle: "handle-0",
-    data: {
-      selectIndex: 0,
-    },
-    markerEnd: {
-      type: MarkerType.ArrowClosed,
-    },
-  },
-  {
-    id: "e4-6",
-    source: "4",
-    target: "6",
-    type: "smoothstep",
-    sourceHandle: "handle-1",
-    data: {
-      selectIndex: 1,
-    },
-    markerEnd: {
-      type: MarkerType.ArrowClosed,
-    },
-  },
+  { id: "e4-1", source: "4", target: "1", animated: true },
+  { id: "e4-2", source: "4", target: "2", animated: true },
 ];
+
+// export const edges = [
+//   { id: "e1-2", source: "1", target: "2", label: "this is an edge label" },
+//   { id: "e1-3", source: "1", target: "3", animated: true },
+//   {
+//     id: "e4-5",
+//     source: "4",
+//     target: "5",
+//     type: "smoothstep",
+//     sourceHandle: "handle-0",
+//     data: {
+//       selectIndex: 0,
+//     },
+//     markerEnd: {
+//       type: MarkerType.ArrowClosed,
+//     },
+//   },
+//   {
+//     id: "e4-6",
+//     source: "4",
+//     target: "6",
+//     type: "smoothstep",
+//     sourceHandle: "handle-1",
+//     data: {
+//       selectIndex: 1,
+//     },
+//     markerEnd: {
+//       type: MarkerType.ArrowClosed,
+//     },
+//   },
+// ];
