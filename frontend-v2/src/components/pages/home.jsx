b170c250d18341649035aa75a64508c3a85f165c
dusklight00 <r.rahul.developer@gmail.com>
2024-02-15 17:49:48 +0530

    home page complete with "tailwindcss" #8

--- frontend-v2/src/components/pages/home.jsx
+++ frontend-v2/src/components/pages/home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="h-full flex border border-1 border-red-700 items-center justify-center">
+      <div className="flex flex-col gap-16 w-full items-center">
+        <div className="text-7xl font-mono font-black">CodeFlow</div>
+        <div className="flex gap-3 w-full justify-center">
+          <input
+            type="text"
+            placeholder="Enter Github URL"
+            className="border rounded-sm px-5 py-3 w-1/3"
+          />
+          <input
+            type="submit"
+            className="bg-emerald-500 px-5 py-3 font-bold rounded-sm"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- frontend-v2/src/main.jsx
+++ frontend-v2/src/main.jsx
@@ -1,12 +1,20 @@
 import React, { StrictMode } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
 import Canvas from "./components/ReactFlow/Canvas";
+import Home from "./components/pages/home";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <Canvas />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/canvas" element={<Canvas />} />
+      </Routes>
+    </BrowserRouter>
+    {/* <Canvas /> */}
   </StrictMode>
 );
