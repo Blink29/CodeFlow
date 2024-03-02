d81c400c11c4bbf9a14eb85f43d9db41328b136d
Blink29 <purukumar2905@gmail.com>
2024-03-02 21:57:36 +0530

    added the function ranking page #18

--- frontend/src/components/pages/functionRanking.jsx
+++ frontend/src/components/pages/functionRanking.jsx
@@ -1,14 +1,72 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./functionRanking.css";
 
 const FunctionRanking = () => {
-    const location = useLocation();
-    const data = location.state
-    console.log(data);
+  const location = useLocation();
+  const data = location.state;
+  const [functionData, setFunctionData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (data) {
+      const callCountMap = {};
+
+      data.forEach((func) => {
+        func.functions_called.forEach((id) => {
+          callCountMap[id] = (callCountMap[id] || 0) + 1;
+        });
+      });
+
+      const updatedFunctionData = data.map((func) => ({
+        ...func,
+        timesCalled: callCountMap[func.id] || 0,
+      })).sort((a, b) => b.timesCalled - a.timesCalled);
+
+      setFunctionData(updatedFunctionData);
+      setIsLoading(true);
+    }
+  }, [data]);
+
+  console.log(functionData);
+
+  const getCallingFunctions = (funcId) => {
+    const callingFunctions = functionData.filter((func) =>
+      func.functions_called.includes(funcId)
+    );
+    return callingFunctions.map((func) => func.function_name).join(", ");
+  };
 
   return (
-    <div>FunctionRanking</div>
-  )
-}
+    <>
+      <div className="container mx-auto">
+        <div className="text-center font-bold text-4xl text-lime-700 mb-4">
+          Function Rankings
+        </div>
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th>Function Name</th>
+              <th>Times Called</th>
+              <th>Called In Functions</th>
+              <th>File Path</th>
+            </tr>
+          </thead>
+          <tbody>
+            {isLoading &&
+              functionData.map((func, index) => (
+                <tr key={index}>
+                  <td>{func.function_name}</td>
+                  <td>{func.timesCalled}</td>
+                  <td>{getCallingFunctions(func.id)}</td>
+                  <td>{func.file_path}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default FunctionRanking
+export default FunctionRanking;
